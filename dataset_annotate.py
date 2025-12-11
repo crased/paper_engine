c947099146ebdf418d87fc466b9380f168041d8f
crazed <75748255+crased@users.noreply.github.com>
2025-12-11 16:52:53 -0500

    fixs

--- dataset_annotate.py
+++ dataset_annotate.py
@@ -4,10 +4,10 @@ import json
 # set url for label studio
 label_studio_url = "http://localhost:8080"
 # set api key for label studio
-label_studio_api_key = ""
+label_studio_api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6ODA3MjY4OTgyMCwiaWF0IjoxNzY1NDg5ODIwLCJqdGkiOiJhOWQ3ZjgzNjYwMDU0NDQ5YjZhYjVlMDI4YjY1ZGRkNSIsInVzZXJfaWQiOiIxIn0.npUJ3DboN9IQjkx3igOO41dhdSjwtdoOuIZag4enfg4"
 
 # connect to label studio API
-cli = LabelStudio(base_url=label_studio_url, api_key=label_studio_api_key)
+client = LabelStudio(base_url=label_studio_url, api_key=label_studio_api_key)
 # A basic request to verify connection is working
 me = client.users.whoami()
 
