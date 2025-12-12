d9af25a12f5882e520be621fbe8936d09ac3e72f
crazed <75748255+crased@users.noreply.github.com>
2025-12-12 17:21:47 -0500

    more fixes

--- dataset_annotate.py
+++ dataset_annotate.py
@@ -1,10 +1,7 @@
 import api_key
-# Define the URL where Label Studio is accessible
-LABEL_STUDIO_URL = 'get_url'
-
-# API key can be either your personal access token or legacy access token
-LABEL_STUDIO_API_KEY = 'key'
-
-# Import the SDK and the client module
 from label_studio_sdk import LabelStudio
-client = LabelStudio(base_url=LABEL_STUDIO_URL, api_key=LABEL_STUDIO_API_KEY)
+
+client = LabelStudio(
+    base_url='http://localhost:8080',  
+    api_key="key",
+)
