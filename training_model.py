f8f150d4e264ec273810dad4d4291abb6bece8c2
crased <hutchersongabriel9@gmail.com>
2026-01-24 19:50:26 -0500

    publishable

--- training_model.py
+++ training_model.py
@@ -1,5 +1,11 @@
 from ultralytics import YOLO
-
+import torch
 def model(self):
    model = YOLO("yolov8n.pt")
-   model.train(data="/dataset")
+   model.train(data="/dataset",
+               epochs=5,
+
+                               
+                             
+                             
+                             )
